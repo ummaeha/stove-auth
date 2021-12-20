74ac218265cf8aa49edecaa83ed2fa41bb41b361
ummaeha <raya000@naver.com>
2021-12-20 21:54:45 +0900

    merge conflicts 나머지 해결

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,5 @@
 const mysql = require('mysql2');
-<<<<<<< HEAD
 require("./env.js")
-=======
->>>>>>> 913f11d2361267d37dea6f33962d02348e52f147
 
 const db = mysql.createConnection({
     host : process.env.DB_HOST,
